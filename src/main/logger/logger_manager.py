c9c6c25addae991e5b2ce6fd6ec028cac870c883
jd-35656 <justjd35656@gmail.com>
2024-01-27 18:35:57 +0530

    main: Add init_app method to Logger class

--- src/main/logger/logger_manager.py
+++ src/main/logger/logger_manager.py
@@ -15,6 +15,8 @@ Create an instance of Logger to initialize the CineBookAPI Flask application:
 
     >>> from logger_manager import Logger
     >>> logger_instance = Logger()
+    >>> wit = Flask(__name__)
+    >>> logger_instance.init_app(wit)
     >>> # You now have a Logger instance for managing logs in the CineBookAPI.
 
 Initialize Logger with a Flask app (e.g., wit) for customized configurations:
@@ -42,6 +44,11 @@ class Logger:
         An optional Flask application instance. If provided, the logger will
         be configured to work with the given Flask app.
 
+    Methods
+    -------
+    init_app(app: Flask) -> None:
+        Initialize the Logger with a Flask app.
+
     Example
     -------
     Create an instance of Logger to initialize the CineBookAPI Flask
@@ -49,6 +56,8 @@ class Logger:
 
     >>> from logger_manager import Logger
     >>> logger_instance = Logger()
+    >>> wit = Flask(__name__)
+    >>> logger_instance.init_app(wit)
     >>> # You now have a Logger instance for managing logs in the CineBookAPI.
 
     Initialize Logger with a Flask app (e.g., wit) for customized
@@ -67,3 +76,28 @@ class Logger:
         self._logger: Optional[logging.Logger] = None
         self._handler: Optional[logging.Handler] = None
         self._formatter: Optional[logging.Formatter] = None
+
+        if self._app is not None:
+            self.init_app(self._app)
+
+    def init_app(self, app: Flask) -> None:
+        """
+        Initialize the Logger with a Flask app.
+
+        Parameters
+        ----------
+        app : Flask
+            Flask application instance.
+        """
+        self._logger = logging.getLogger("app")
+        self._logger.setLevel(logging.DEBUG)
+
+        self._handler = logging.FileHandler("logs.log")
+        self._handler.setLevel(app.config["LOG_LEVEL"])  # type: ignore
+
+        self._formatter = logging.Formatter(
+            "%(asctime)s - %(levelname)s - %(name)s - %(message)s"
+        )
+        self._handler.setFormatter(self._formatter)
+
+        self._logger.addHandler(self._handler)
