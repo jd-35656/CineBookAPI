8a6db13dfca954cf11e29dab94da56c82455bb7f
jd-35656 <justjd35656@gmail.com>
2024-01-29 03:57:33 +0530

    main: Add prod config class.

---
+++ src/main/config/prod_config.py
@@ -0,0 +1,16 @@
+"""
+Prod Config Module
+===================
+
+Classes
+-------
+ProdConfig: BaseConfig
+    Prod configuration class.
+"""
+from src.main.config.base_config import BaseConfig
+
+
+class ProdConfig(BaseConfig):  # pylint: disable=too-few-public-methods
+    """
+    Prod configuration class.
+    """
