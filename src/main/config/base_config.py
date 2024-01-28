9995b5892c0ba8fd6e2575425fd3ec86f7d46c90
jd-35656 <justjd35656@gmail.com>
2024-01-29 03:51:55 +0530

    main: Add base config class.

---
+++ src/main/config/base_config.py
@@ -0,0 +1,15 @@
+"""
+Base Config Module
+===================
+
+Classes
+-------
+BaseConfig: class
+    Base configuration class.
+"""
+
+
+class BaseConfig:  # pylint: disable=too-few-public-methods
+    """
+    Base configuration class.
+    """
