f3771a45b72cd6d216da997018c1d02d70b9853f
jd-35656 <justjd35656@gmail.com>
2024-01-29 03:56:01 +0530

    main: Add dev config class.

---
+++ src/main/config/dev_config.py
@@ -0,0 +1,16 @@
+"""
+Dev Config Module
+===================
+
+Classes
+-------
+DevConfig: BaseConfig
+    Dev configuration class.
+"""
+from src.main.config.base_config import BaseConfig
+
+
+class DevConfig(BaseConfig):  # pylint: disable=too-few-public-methods
+    """
+    Dev configuration class.
+    """
