7fc7de12e12bdf34af1e250a8b26399b1d284fbc
jd-35656 <justjd35656@gmail.com>
2024-01-30 00:59:37 +0530

    main: Add database uri config.

--- src/main/config/base_config.py
+++ src/main/config/base_config.py
@@ -9,7 +9,13 @@ BaseConfig: class
 """
 
 
+import os
+from typing import Optional
+
+
 class BaseConfig:  # pylint: disable=too-few-public-methods
     """
     Base configuration class.
     """
+
+    DATABASE_URI: Optional[str] = os.environ.get("DATABASE_URI")
