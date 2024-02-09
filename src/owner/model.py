67f12c20491003b6265ef0b387a144c36d7e0893
jd-35656 <justjd35656@gmail.com>
2024-02-10 01:37:31 +0530

    owner: Implement owner model.

---
+++ src/owner/model.py
@@ -0,0 +1,79 @@
+"""
+Defines the OwnerModel class representing owners in the system.
+
+This module provides the OwnerModel class, which represents owners in
+the system. It includes attributes such as id, email, phone number,
+password, creation date, and last update date.
+
+Attributes
+----------
+id : Mapped[UUID]
+    The unique identifier for the owner, generated using UUID version 1.
+email : Mapped[str]
+    The email address of the owner.
+phone : Mapped[int]
+    The phone number of the owner.
+password : Mapped[str]
+    The password associated with the owner's account.
+created : Mapped[datetime]
+    The datetime when the owner's account was created.
+updated : Mapped[datetime]
+    The datetime when the owner's account was last updated.
+"""
+
+from datetime import datetime
+from uuid import UUID, uuid1
+
+from sqlalchemy import Integer, String, Uuid
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.main.database.base import Base
+
+
+class OwnerModel(Base):  # pylint: disable= too-few-public-methods
+    """
+    Represents an owner in the system.
+
+    Attributes
+    ----------
+    id : Mapped[UUID]
+        The unique identifier for the owner, generated using UUID version 1.
+    email : Mapped[str]
+        The email address of the owner.
+    phone : Mapped[int]
+        The phone number of the owner.
+    password : Mapped[str]
+        The password associated with the owner's account.
+    created : Mapped[datetime]
+        The datetime when the owner's account was created.
+    updated : Mapped[datetime]
+        The datetime when the owner's account was last updated.
+    """
+
+    __tablename__ = "owners"
+
+    id: Mapped[UUID] = mapped_column(
+        Uuid(as_uuid=True),
+        primary_key=True,
+        default=uuid1(),
+    )
+    email: Mapped[str] = mapped_column(
+        String(20),
+        nullable=True,
+        unique=True,
+    )
+    phone: Mapped[int] = mapped_column(
+        Integer,
+        unique=True,
+    )
+    password: Mapped[str] = mapped_column(
+        String(64),
+        nullable=True,
+    )
+    created: Mapped[datetime] = mapped_column(
+        default=datetime.now().isoformat(),
+    )
+    updated: Mapped[datetime] = mapped_column(
+        default=datetime.now().isoformat(),
+        onupdate=datetime.now().isoformat(),
+    )
