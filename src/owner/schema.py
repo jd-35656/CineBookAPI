7dde4b3be102838d66460c6865119d5617e380a8
jd-35656 <justjd35656@gmail.com>
2024-02-10 02:22:00 +0530

    owner: Include Owner update Schema.

--- src/owner/schema.py
+++ src/owner/schema.py
@@ -2,8 +2,8 @@
 Module for defining a schema to represent owners with email,
 phone, and password fields.
 
-This module contains the `OwnerSchema` class, which is a
-Marshmallow schema used for validating and serializing/
+This module contains the `OwnerSchema` and `OwnerUpdateSchema` class,
+which is a Marshmallow schema used for validating and serializing/
 deserializing owner data.
 
 Example
@@ -12,23 +12,32 @@ Define an `OwnerSchema` instance:
 
     >>> owner_schema = OwnerSchema()
 
-Attributes
-----------
-hashlib : module
-    Provides cryptographic hash functions.
-marshmallow : module
-    A library for object serialization and deserialization.
+Define an `OwnerUpdateSchema` instance:
+
+    >>> owner_update_schema = OwnerUpdateSchema()
 
 Classes
 -------
 OwnerSchema : class
     A schema for representing owners with email, phone, and
     password fields.
+
+OwnerUpdateSchema : class
+    A schema for updating owner information with email, phone,
+    and password fields.
+
 """
 
 import hashlib
 
-from marshmallow import Schema, fields, pre_load, validate
+from marshmallow import (
+    Schema,
+    ValidationError,
+    fields,
+    pre_load,
+    validate,
+    validates_schema,
+)
 
 
 class OwnerSchema(Schema):
@@ -77,6 +86,91 @@ class OwnerSchema(Schema):
         ),
         required=True,
     )
+
+    @pre_load
+    def process_input(self, data, **kwargs):  # pylint: disable=unused-argument
+        """
+        Pre-processing method to hash the password before
+        loading into the schema.
+
+        Parameters
+        ----------
+        data : dict
+            Input data containing password.
+
+        Returns
+        -------
+        dict
+            Processed data with hashed password.
+        """
+        if "password" in data:
+            password_bytes = data["password"].encode("utf-8")
+            hashed_password = hashlib.sha256(password_bytes).hexdigest()
+            data["password"] = hashed_password
+        return data
+
+
+class OwnerUpdateSchema(Schema):
+    """
+    Schema for updating owner information with email, phone, and
+    password fields.
+
+    Attributes
+    ----------
+    email : marshmallow.fields.Email
+        Email field for owner's email address (optional).
+    phone : marshmallow.fields.Int
+        Numeric field for owner's phone number (optional).
+        Must be within the range of 10 digits.
+    password : marshmallow.fields.Str
+        String field for owner's password (optional).
+        Must be between 8 and 16 characters long.
+
+    Methods
+    -------
+    validate_at_least_one_field(data, **kwargs)
+        Method to validate that at least one field (email, phone, password)
+        is provided.
+    process_input(data, **kwargs)
+        Pre-processing method to hash the password before loading into
+        the schema.
+    """
+
+    email = fields.Email()
+    phone = fields.Int(
+        validate=validate.Range(
+            min=1000000000,
+            max=9999999999,
+        ),
+    )
+    password = fields.Str(
+        validate=validate.Length(
+            min=8,
+            max=16,
+        ),
+    )
+
+    @validates_schema
+    def validate_at_least_one_field(
+        self, data, **kwargs
+    ):  # pylint: disable=unused-argument
+        """
+        Validate that at least one field (email, phone, password) is provided.
+
+        Parameters
+        ----------
+        data : dict
+            Input data containing owner information.
+
+        Raises
+        ------
+        ValidationError
+            If none of the fields (email, phone, password) is provided.
+        """
+        if not any(field in data for field in ["email", "phone", "password"]):
+            raise ValidationError(
+                "At least one field (email, phone, password) must be provided."
+            )
 
     @pre_load
     def process_input(self, data, **kwargs):  # pylint: disable=unused-argument
