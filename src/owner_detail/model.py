584b8b6e22f7cc35004d7c4bcfd2c2c194b97fcb
jd-35656 <justjd35656@gmail.com>
2024-02-10 11:32:34 +0530

    owner_detail: Include owner details module.

---
+++ src/owner_detail/model.py
@@ -0,0 +1,64 @@
+"""
+Module containing SQLAlchemy model for Owner details.
+
+This module defines the SQLAlchemy model `OwnerModel` to
+represent details of owners, including their identification,
+personal information, and timestamps.
+
+Classes:
+    OwnerDetailModel: SQLAlchemy model representing owner details.
+"""
+
+from datetime import datetime
+from typing import Dict, Union
+from uuid import UUID, uuid1
+
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.main.database.base import Base
+
+
+class OwnerDetailModel(Base):  # pylint: disable= too-few-public-methods
+    """
+    SQLAlchemy model for representing owner details.
+
+    Attributes:
+        id (Mapped[UUID]): Unique identifier for the owner.
+        name (Mapped[str]): Name of the owner.
+        dob (Mapped[datetime]): Date of birth of the owner.
+        gender (Mapped[str]): Gender of the owner.
+        address (Mapped[Dict[str, Union[str, int]]]): Address
+            details of the owner.
+        created (Mapped[datetime]): Timestamp indicating when
+            the record was created.
+        updated (Mapped[datetime]): Timestamp indicating when
+            the record was last updated.
+
+    """
+
+    __tablename__ = "owner_details"
+
+    id: Mapped[UUID] = mapped_column(
+        primary_key=True,
+        default=uuid1(),
+    )
+    name: Mapped[str] = mapped_column(
+        nullable=True,
+        unique=True,
+    )
+    dob: Mapped[datetime] = mapped_column(
+        nullable=False,
+    )
+    gender: Mapped[str] = mapped_column(
+        nullable=False,
+    )
+    address: Mapped[Dict[str, Union[str, int]]] = mapped_column(
+        nullable=False,
+    )
+    created: Mapped[datetime] = mapped_column(
+        default=datetime.now().isoformat(),
+    )
+    updated: Mapped[datetime] = mapped_column(
+        default=datetime.now().isoformat(),
+        onupdate=datetime.now().isoformat(),
+    )
