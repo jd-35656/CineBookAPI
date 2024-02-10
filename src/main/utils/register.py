ed7efc35d520a4bf7513ad876eca79b3ed42ee1c
jd-35656 <justjd35656@gmail.com>
2024-02-10 20:28:06 +0530

    main: Implement register method.

---
+++ src/main/utils/register.py
@@ -0,0 +1,32 @@
+"""Utility function for registering Flask blueprints."""
+
+from typing import Dict, Union
+
+from flask import Blueprint
+
+
+# pylint: disable=unused-argument
+def register(
+    blueprint: Blueprint,
+    endpoint: str,
+    name: str,
+) -> Dict[str, Union[str, Blueprint]]:
+    """
+    Register a Flask blueprint with additional metadata.
+
+    Parameters:
+        blueprint (Blueprint): The Flask blueprint to register.
+        endpoint (str): The endpoint at which the blueprint will be accessible.
+        name (str): The name of the blueprint.
+
+    Returns:
+        dict: A dictionary containing metadata about the registered blueprint.
+            - 'blueprint': The Flask blueprint object.
+            - 'endpoint': The endpoint at which the blueprint is accessible.
+            - 'name': The name of the blueprint.
+    """
+    return {
+        "blueprint": blueprint,
+        "endpoint": endpoint,
+        "name": name,
+    }
