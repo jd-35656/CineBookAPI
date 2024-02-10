7a316e1bbf8d639ef6476f770e42e12d901a89e7
jd-35656 <justjd35656@gmail.com>
2024-02-10 20:50:03 +0530

    main: Integrate owner auth model.

--- src/main/models.py
+++ src/main/models.py
@@ -1,4 +1,5 @@
 # pylint: disable=all # type: ignore
 # Add '# noqa' at end of each model file import.
 from src.owner import model as owner_model  # noqa
+from src.owner_auth import model as owner_auth_model  # noqa
 from src.owner_detail import model as owner_detail_model  # noqa
